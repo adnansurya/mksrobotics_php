9b6e04b90d04bb106177c840dd67213b37ce29ee
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-08-15 21:10:07 +0800

    fix pagination

--- produk.php
+++ produk.php
@@ -4,7 +4,8 @@ session_start();
 include('partials/global.php'); 
 include('access/session.php');
 
-
+$halaman = isset($_GET["per"]) ? (int)$_GET["per"] : 10;
+$kategori = isset($_GET["kat"]) ? $_GET["kat"] : "SEMUA KATEGORI";
 ?>
 
         
@@ -26,6 +27,42 @@ include('access/session.php');
                         <h1 class="mt-4">Produk</h1>
                         <hr>
                         <div class="row">
+                            <div class="col-md-4">
+                            <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <label class="input-group-text" for="inputGroupSelect01">Kategori</label>
+                                    </div>
+                                    <select class="custom-select" onChange="window.location.href=this.value">
+                                        <option value="?kat=SEMUA KATEGORI&per=<?php echo $halaman; ?>" <?php if($kategori=="SEMUA KATEGORI"){ echo 'selected';}?>>SEMUA KATEGORI</option>
+                                        <?php   
+                                        $dbCategory = new SQLite3('uploads/category.db');
+                                        if(!$dbProduct){
+                                            echo '<p>Error</p>';
+                                        }
+                                        $categories = $dbCategory->query("select * from table_category ORDER BY category_name");
+                                        while($row = $categories->fetchArray(SQLITE3_ASSOC) ) {       
+                                            echo '<option value="?kat='.$row['category_name'].'&per='.$halaman.'"'; if($kategori==$row['category_name']){ echo 'selected';}
+                                            echo '>'.$row['category_name'].'</option>';
+                                        }
+                                        ?>
+                                       -->
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-md-4 offset-md-4">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <label class="input-group-text" for="inputGroupSelect01">Tampilkan</label>
+                                    </div>
+                                    <select class="custom-select" onChange="window.location.href=this.value">
+                                        <option value="?per=10" <?php if($halaman==10){ echo 'selected';}?>>10 Item per Halaman</option>
+                                        <option value="?per=20" <?php if($halaman==20){ echo 'selected';}?>>20 Item per Halaman</option>
+                                        <option value="?per=40" <?php if($halaman==40){ echo 'selected';}?>>40 Item per Halaman</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
                         
                         <?php
 
@@ -34,16 +71,21 @@ include('access/session.php');
                         if(!$dbProduct){
                             echo '<p>Error</p>';
                         }
-                        $halaman = 10;
-                        $page = isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
+                       
+                        $page = isset($_GET["hal"]) ? (int)$_GET["hal"] : 1;
                         $mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
                         // $result = $dbProduct->query("SELECT * FROM table_product ORDER BY product_name");
                         $total = $dbProduct->querySingle("SELECT COUNT(*) as count FROM table_product");                        
-                        $pages = ceil($total/$halaman);            
-                        $queries = $dbProduct->query("select * from table_product ORDER BY product_name ASC LIMIT ".$mulai.", ".$halaman);
+                          
+                        $sql_syntax = "select * from table_product ORDER BY product_name ASC LIMIT ".$mulai.", ".$halaman;
+                        if($kategori != 'SEMUA KATEGORI'){
+                            $total = $dbProduct->querySingle("SELECT COUNT(*) as count FROM table_product where product_category_name = '".$kategori."'");
+                            $sql_syntax = "select * from table_product where product_category_name = '".$kategori. "' ORDER BY product_name ASC LIMIT ".$mulai.", ".$halaman;
+                        }     
+                        $pages = ceil($total/$halaman);     
+                        $queries = $dbProduct->query($sql_syntax);
                         $no =$mulai+1;
-
-                        // $ret = $dbProduct->query('SELECT * from table_product ORDER BY product_name');
+                        
                         $prod = 0;
                         while($row = $queries->fetchArray(SQLITE3_ASSOC) ) {                          
                     echo '<div class="col-lg-3 col-md-4 col-sm-6 mb-4">
@@ -84,16 +126,13 @@ include('access/session.php');
                                 <nav aria-label="Page navigation example">
                                     <div class="mx-auto">
                                     <ul class="pagination">
-                                        <li class="page-item <?php if ($page==1){echo "disabled";}?>"><a class="page-link" href="?halaman=<?php echo $page-1; ?>"">Sebelumnya</a></li>
+                                        <li class="page-item <?php if ($page==1){echo "disabled";}?>"><a class="page-link" href="?hal=<?php echo ($page-1)."&per=".$halaman."&kat=".$kategori; ?>"">Sebelumnya</a></li>
                                         <?php for ($i=1; $i<=$pages ; $i++){ ?>
                                             
-                                            <li class="page-item <?php if ($page==$i){echo "active";}?>"><a class="page-link" href="?halaman=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                                            <li class="page-item <?php if ($page==$i){echo "active";}?>"><a class="page-link" href="?hal=<?php echo $i."&per=".$halaman."&kat=".$kategori; ?>"><?php echo $i; ?></a></li>
                                             
-                                        <?php } ?>
-                                        
-                                      <!-- <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>--> 
-                                        <li class="page-item <?php if ($page==$pages){echo "disabled";}?>"><a class="page-link" href="?halaman=<?php echo $page+1; ?>"">Selanjutnya</a></li> 
+                                        <?php } ?>                            
+                                        <li class="page-item <?php if ($page==$pages){echo "disabled";}?>"><a class="page-link" href="?hal=<?php echo ($page+1)."&per=".$halaman."&kat=".$kategori; ?>"">Selanjutnya</a></li> 
                                     </ul>
                                     </div>
                                     
