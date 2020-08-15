0b375416dc027f432f72bc807772d90a2f2866fd
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-08-15 20:04:37 +0800

    pagination

--- produk.php
+++ produk.php
@@ -24,17 +24,28 @@ include('access/session.php');
                 <main>
                     <div class="container-fluid">
                         <h1 class="mt-4">Produk</h1>
+                        <hr>
                         <div class="row">
                         
                         <?php
+
 
                         $dbProduct = new SQLite3('uploads/product.db');
                         if(!$dbProduct){
                             echo '<p>Error</p>';
                         }
-                        $ret = $dbProduct->query('SELECT * from table_product');
+                        $halaman = 10;
+                        $page = isset($_GET["halaman"]) ? (int)$_GET["halaman"] : 1;
+                        $mulai = ($page>1) ? ($page * $halaman) - $halaman : 0;
+                        // $result = $dbProduct->query("SELECT * FROM table_product ORDER BY product_name");
+                        $total = $dbProduct->querySingle("SELECT COUNT(*) as count FROM table_product");                        
+                        $pages = ceil($total/$halaman);            
+                        $queries = $dbProduct->query("select * from table_product ORDER BY product_name ASC LIMIT ".$mulai.", ".$halaman);
+                        $no =$mulai+1;
+
+                        // $ret = $dbProduct->query('SELECT * from table_product ORDER BY product_name');
                         $prod = 0;
-                        while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {                          
+                        while($row = $queries->fetchArray(SQLITE3_ASSOC) ) {                          
                     echo '<div class="col-lg-3 col-md-4 col-sm-6 mb-4">
                             <div class="card h-100">                           
                               <div class="card-body">
@@ -67,6 +78,27 @@ include('access/session.php');
 
 
 
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <nav aria-label="Page navigation example">
+                                    <div class="mx-auto">
+                                    <ul class="pagination">
+                                        <li class="page-item"><a class="page-link" href="?halaman=<?php echo $page-1; ?>"">Sebelumnya</a></li>
+                                        <?php for ($i=1; $i<=$pages ; $i++){ ?>
+                                            
+                                            <li class="page-item <?php if ($page==$i){echo "active";}?>"><a class="page-link" href="?halaman=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                                            
+                                        <?php } ?>
+                                        
+                                      <!-- <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                        <li class="page-item"><a class="page-link" href="#">3</a></li>--> 
+                                        <li class="page-item"><a class="page-link" href="?halaman=<?php echo $page+1; ?>"">Selanjutnya</a></li> 
+                                    </ul>
+                                    </div>
+                                    
+                                </nav>
+                            </div>
                         </div>
                         <!-- </div> -->
                     </div>
