b72cf6884f940d09df9f5075461d336507f8210e
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-08-19 17:04:12 +0800

    fix product detail auth issue

--- access/product_detail.php
+++ access/product_detail.php
@@ -6,45 +6,43 @@
           echo '<p>DB Error</p>';
   }
 
-  if(($_SESSION['logged_role'] == 'SU' || $_SESSION['logged_role'] == 'AD')){
+  if((isset($_POST['id']) && isset($_POST['description']) && isset($_POST['url_image'])) &&($_SESSION['logged_role'] == 'SU' || $_SESSION['logged_role'] == 'AD')){
       
-        if(isset($_POST['id']) && isset($_POST['description']) && isset($_POST['url_image'])){
+      
+        
         
 
-          $check_desc = $db->querySingle("SELECT COUNT(*) as count FROM product_details WHERE product_id ='".$_POST['id']."'");
-          if($check_desc==1){
-              $sql = $db->exec("UPDATE product_details 
-              SET image_url='".$_POST['url_image']."', description='".$_POST['description']."' 
-              WHERE product_id=".$_POST['id']);
-          }else{
-              $sql = $db->exec("INSERT INTO product_details (image_url, description, product_id) 
-              VALUES ('".$_POST['url_image']."','".$_POST['description']."',".$_POST['id'].")");
-          }
-          
-          if($sql){
-              header("location:../data_produk.php");      
-              die();
-          }else{
-              echo 'Operasi database gagal';
-          }
-        }elseif(isset($_GET['req_id'])){
-          $resObj = new \stdClass();
-          $check_desc = $db->querySingle("SELECT COUNT(*) as count FROM product_details WHERE product_id ='".$_GET['req_id']."'");
-          if($check_desc==1){
-              $desc = $db->querySingle("SELECT * FROM product_details WHERE product_id=".$_GET['req_id'], true);
-              $resObj -> result = "success";
-              $resObj -> data = $desc;
-          }else{
-              $resObj -> result = "unknown";
-          }
-          echo json_encode($resObj);
-        }
-        else{
-          echo 'Data tidak Lengkap';
-        }
+      $check_desc = $db->querySingle("SELECT COUNT(*) as count FROM product_details WHERE product_id ='".$_POST['id']."'");
+      if($check_desc==1){
+          $sql = $db->exec("UPDATE product_details 
+          SET image_url='".$_POST['url_image']."', description='".$_POST['description']."' 
+          WHERE product_id=".$_POST['id']);
+      }else{
+          $sql = $db->exec("INSERT INTO product_details (image_url, description, product_id) 
+          VALUES ('".$_POST['url_image']."','".$_POST['description']."',".$_POST['id'].")");
+      }
+      
+      if($sql){
+          header("location:../data_produk.php");      
+          die();
+      }else{
+          echo 'Operasi database gagal';
+      }
+    
       
       
     
+  }elseif(isset($_GET['req_id']) &&($_SESSION['logged_role'] == 'SU' || $_SESSION['logged_role'] == 'AD')){
+    $resObj = new \stdClass();
+    $check_desc = $db->querySingle("SELECT COUNT(*) as count FROM product_details WHERE product_id ='".$_GET['req_id']."'");
+    if($check_desc==1){
+        $desc = $db->querySingle("SELECT * FROM product_details WHERE product_id=".$_GET['req_id'], true);
+        $resObj -> result = "success";
+        $resObj -> data = $desc;
+    }else{
+        $resObj -> result = "unknown";
+    }
+    echo json_encode($resObj);
   }elseif(isset($_GET['get_id'])){
     $resObj = new \stdClass();    
 
