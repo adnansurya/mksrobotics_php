174fb6a3daba6a8ed018c8281c2d2a0d133f2cad
Adnan <muh.adnansuryaazis@gmail.com>
2021-04-12 09:18:55 +0800

    login page

--- login.php
+++ login.php
@@ -12,57 +12,59 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php include('partials/global.php') ?>
-        <?php include('partials/head.php'); ?>
+<head>
+    <?php include('partials/global.php') ?>
+    <?php include('partials/head.php'); ?>
         
-        <title><?php echo $webname; ?> - Login</title>         
-    </head>
-    <body class="bg-primary">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-5">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
-                                    <div class="card-body">
-                                        <form action="access/signin.php" method="post">
-                                            <div class="form-group">
-                                                <label class="small mb-1" for="inputEmailAddress">Email atau Username</label>
-                                                <input class="form-control py-4" id="inputEmailAddress" type="text" placeholder="Masukkan Email" name="user"/>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="small mb-1" for="inputPassword">Password</label>
-                                                <input class="form-control py-4" id="inputPassword" type="password" placeholder="Masukkan Password" name="pass"/>
-                                            </div>                                            
-                                            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">                                                
-                                                <button class="btn btn-primary"  type="submit">Login</button>
-                                            </div>
-                                        </form>
-                                    </div>                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </main>
+    <title><?php echo $webname; ?> - Produk</title>  
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center p-4">
+      <a href="index.php" class="h1"><img src="image/logo.jpg" alt="logo" class="img-fluid"></a>
+    </div>
+    <div class="card-body">
+        <p class="login-box-msg">Login untuk melanjutkan sesi Anda</p>
+        <form action="access/signin.php" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Email atau Username" id="inputEmailAddress" name="user" >
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
             </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
+          </div>
         </div>
-       <?php include('partials/scripts.php'); ?>
-    </body>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" id="inputPassword" name="pass">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">Login</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      
+    </div>
+    <!-- /.card-body -->
+  </div>
+  <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
 </html>
