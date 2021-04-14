45c8dd083fb409d23644d239863a989a8f0c200a
Adnan <muh.adnansuryaazis@gmail.com>
2021-04-14 12:02:20 +0800

    add favicon

--- partials/head.php
+++ partials/head.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico"/>
 <!-- <script src="js/font-awesome.all.min.js" crossorigin="anonymous"></script> -->
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
