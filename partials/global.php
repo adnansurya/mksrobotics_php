42a224930f7376c5ae40c60f825b57e24fd3b78e
Adnan <muh.adnansuryaazis@gmail.com>
2021-04-24 22:55:40 +0800

    random string function

--- partials/global.php
+++ partials/global.php
@@ -80,4 +80,17 @@
         return number_format($inputPrice, 0, ",", ".");
     
     }
+
+    function getRandomStrings($str_length){
+        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $input_length = strlen($permitted_chars);
+        $random_string = '';
+        for($i = 0; $i < $str_length; $i++) {
+            $random_character = $permitted_chars[mt_rand(0, $input_length - 1)];
+            $random_string .= $random_character;
+        }
+    
+        return $random_string;
+        
+    }
 ?>
